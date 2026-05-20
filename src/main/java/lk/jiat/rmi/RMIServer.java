c884cd3f64129352c672326206ac309449a03c8e
Sachith Gunarathna <106460379+Sachith-Gunarathna@users.noreply.github.com>
2026-05-20 11:54:54 +0530

    v1.0.3

--- src/main/java/lk/jiat/rmi/RMIServer.java
+++ src/main/java/lk/jiat/rmi/RMIServer.java
@@ -1,9 +1,21 @@
 package lk.jiat.rmi;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class RMIServer {
 
     public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(6666);
+            registry.bind("message_service",new MessageImpl());
 
+            System.out.println("RMI server started...");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
